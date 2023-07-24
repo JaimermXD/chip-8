5b41c48f747ebfadac919feeba065e72c2b14896
Jaime Redondo <jairegra@gmail.com>
2023-07-24 18:16:42 +0200

    Implement SDL init and cleanup, plus basic config

--- chip8.c
+++ chip8.c
@@ -3,13 +3,109 @@
 /* -------------------------------------------------------------------------- */
 
 #include <stdio.h>
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+
+/* -------------------------------------------------------------------------- */
+/*                                    DATA                                    */
+/* -------------------------------------------------------------------------- */
+
+// Config
+uint32_t width;
+uint32_t height;
+uint32_t scale;
+
+// SDL
+SDL_Window *window;
+SDL_Renderer *renderer;
+
+/* -------------------------------------------------------------------------- */
+/*                                   CONFIG                                   */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Set config defaults and override from args
+ * @param argc Number of args
+ * @param argv Args list
+ * @return Whether setup was successful
+*/
+bool set_config(int argc, char **argv) {
+    // Set defaults
+    width = 64;
+    height = 32;
+    scale = 15;
+
+    // TODO: Override from args
+    for (int i = 0; i < argc; i++) {
+        (void)argv[i];
+    }
+
+    return true;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                     SDL                                    */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Initialize SDL susbsytems and components
+ * @return Whether initialization was successful
+*/
+bool init_sdl() {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+        fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+        return false;
+    }
+
+    window = SDL_CreateWindow(
+        "CHIP-8 Emulator",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        width * scale,
+        height * scale,
+        0
+    );
+    if (!window) {
+        fprintf(stderr, "Unable to create window: %s\n", SDL_GetError());
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        fprintf(stderr, "Unable to create renderer: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * Final SDL cleanup
+*/
+void clean_sdl() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
 /* -------------------------------------------------------------------------- */
 /*                                    MAIN                                    */
 /* -------------------------------------------------------------------------- */
 
+/**
+ * Application entry point
+ * @param argc Number of args
+ * @param argv Args list
+ * @return Exit code
+*/
 int main(int argc, char **argv) {
-    printf("Hello, World!\n");
+    if (!set_config(argc, argv)) return EXIT_FAILURE;
+    if (!init_sdl()) return EXIT_FAILURE;
 
-    return 0;
+    // TODO: main loop
+
+    clean_sdl();
+
+    return EXIT_SUCCESS;
 }
