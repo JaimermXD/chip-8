6e3ecb92836ad98052db477673de971ad671904e
Jaime Redondo <jairegra@gmail.com>
2023-07-24 18:54:45 +0200

    Create CHIP-8 components

--- chip8.c
+++ chip8.c
@@ -29,6 +29,38 @@ SDL_Renderer *renderer;
 
 // Emulator
 state_t state;
+uint8_t memory[4096];
+uint8_t V[16];
+uint16_t stack[16];
+uint8_t sp;
+uint16_t PC;
+uint16_t I;
+uint8_t DT;
+uint8_t ST;
+bool display[64 * 32];
+bool keypad[16];
+char *rom;
+
+// Constants
+const uint16_t entry_point = 0x200;
+const uint8_t font[] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
 
 /* -------------------------------------------------------------------------- */
 /*                                   CONFIG                                   */
@@ -158,8 +190,10 @@ void clean_sdl() {
  * Initialize CHIP-8 emulator and set up initial state
  * @return Whether initialization was successful
 */
-bool init_emulator() {
+bool init_emulator(char *rom_name) {
     state = RUNNING;
+    PC = entry_point;
+    rom = rom_name;
 
     return true;
 }
@@ -175,8 +209,13 @@ bool init_emulator() {
  * @return Exit code
 */
 int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s ROM_NAME\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
     if (!set_config(argc, argv)) return EXIT_FAILURE;
-    if (!init_emulator()) return EXIT_FAILURE;
+    if (!init_emulator(argv[1])) return EXIT_FAILURE;
     if (!init_sdl()) return EXIT_FAILURE;
 
     // Main loop
